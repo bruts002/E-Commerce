f9f99ba218b85d8737245bdb53c4b93f09b96cf3
bruts002 <mikebrutskiy@gmail.com>
2016-12-26 22:33:00 -0600

    Modularizing menu bar css to sass

--- templates/menubar.php
+++ templates/menubar.php
@@ -5,7 +5,7 @@ $level = getPath(isset($dir_level) ? $dir_level : 0);
 <!--Navigation Bar starts-->
 <div class="menubar">
 
-    <ul id="menu">
+    <ul class="menu">
         <li><a href="<?php echo $level ?>index.php">Home</a></li>
         <li><a href="<?php echo $level ?>all_products.php">All Products</a></li>
         <li><a href="<?php echo $level ?>customer/my_account.php">My Account</a></li>
@@ -14,7 +14,7 @@ $level = getPath(isset($dir_level) ? $dir_level : 0);
     
     </ul>
     
-    <div id="form">
+    <div class="form">
         <form method="get" action="<?php echo $level ?>results.php" enctype="multipart/form-data">
             <input type="text"   name="user_query" placeholder="Search a Product" / >
             <input type="submit" name="search"     value="Search" />
