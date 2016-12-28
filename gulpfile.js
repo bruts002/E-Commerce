7088f96f4a9bf565d5ac052891751f4933896cf8
bruts002 <mikebrutskiy@gmail.com>
2016-12-27 23:18:30 -0600

    Removing browser sync from workflow
    Refreshing is not hard. I can't delete the browserSync folder in
    node_modules; getting a windows error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var browserSync = require('browser-sync').create();
-
-gulp.task('serve', function() {
-	browserSync.init({
-        proxy:'localhost/ecommerce',
-        port:801
-	});
-
-	gulp.watch('./**/*.css').on('change', browserSync.reload);
-	gulp.watch('./**/*.html').on('change', browserSync.reload);
-	gulp.watch('./**/*.php').on('change', browserSync.reload);
-	gulp.watch('./js/*.js').on('change', browserSync.reload);
-});
 
 function buildSass() {
 	sass('./styles/main.scss', {sourcemap: true})
 		.on('error', sass.logError)
 		.pipe(gulp.dest('./styles/'))
-};
+}
 
 gulp.task('sass', function() {
 	gulp.watch('./styles/**/*.scss').on('change', function (event) {
 		buildSass();
 	});
-})
+});
 
 gulp.task('build-sass', function() {
 	buildSass();
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['build-sass']);
